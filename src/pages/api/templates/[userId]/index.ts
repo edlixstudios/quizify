2c24d7fead39f90d12e7a8e0cb71223f690d329c
Eduard Albrecht <eddysoulo91@gmail.com>
2023-03-12 18:35:41 +0100

    feat: add migration file

--- src/pages/api/templates/[userId]/index.ts
+++ src/pages/api/templates/[userId]/index.ts
@@ -1,57 +1,69 @@
 import { Template } from "@prisma/client";
 import { NextApiRequest, NextApiResponse } from "next";
+import { TemplateTypes } from "root/lib/templateClass";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    switch (req.method) {
-        case "GET":
-            await getHandler(req, res);
-            break;
-        case "POST":
-            await postHandler(req, res);
-            break;
-    }
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  switch (req.method) {
+    case "GET":
+      await getHandler(req, res);
+      break;
+    case "POST":
+      await postHandler(req, res);
+      break;
+  }
 }
 
-async function getHandler(req: NextApiRequest, res: NextApiResponse<Template[]>) {
-    const { userId } = req.query as { userId: string };
+async function getHandler(
+  req: NextApiRequest,
+  res: NextApiResponse<Template[]>
+) {
+  const { userId } = req.query as { userId: string };
 
-    const templates = await prisma?.template.findMany({
-        where: {
-            userId,
-        },
-    });
+  const templates = await prisma?.template.findMany({
+    where: {
+      userId,
+    },
+  });
 
-    res.send(templates!);
+  res.send(templates!);
 }
 
 interface ResponseMessage {
-    success: boolean;
-    msg: string;
+  success: boolean;
+  msg: string;
 }
 
-async function postHandler(req: NextApiRequest, res: NextApiResponse<ResponseMessage>) {
-    const { userId } = req.query as { userId: string };
+async function postHandler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseMessage>
+) {
+  const { userId } = req.query as { userId: string };
 
-    const { id, title, finish, template } = req.body as {
-        id: string;
-        title: string;
-        finish: boolean;
-        template: {};
-    };
+  const { id, title, finish, template, type } = req.body as {
+    id: string;
+    title: string;
+    finish: boolean;
+    type: TemplateTypes;
+    template: {};
+  };
 
-    const createNewTemplate = await prisma?.template.create({
-        data: {
-            id,
-            userId,
-            title,
-            finish,
-            template,
-        },
-    });
+  const createNewTemplate = await prisma?.template.create({
+    data: {
+      id,
+      userId,
+      title,
+      finish,
+      template,
+      type,
+    },
+  });
 
-    if (createNewTemplate) {
-        res.send({ success: true, msg: "Tempalte Created" });
-    } else {
-        res.send({ success: false, msg: "Error on creating template" });
-    }
+  if (createNewTemplate) {
+    res.send({ success: true, msg: "Template Created" });
+  } else {
+    res.send({ success: false, msg: "Error on creating template" });
+  }
 }
